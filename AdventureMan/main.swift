c88078c2f0cac4536cda3ffb69f6b36aca2adcd0
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-09-22 23:53:42 -0400

    Create Xcode project with command line tool target

---
+++ AdventureMan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventureMan
+//
+//  Created by Nikolai Vazquez on 9/22/15.
+//  Copyright © 2015 CCHS Coding Club. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
